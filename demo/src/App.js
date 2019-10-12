e328ba1929808fdaf06e985ac02ae5c6ab3a4c79
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-12 14:32:16 -0500

    Adding legend and content to demo page.

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,8 +4,14 @@ import ParticlesPointerTracker from './particlesComponentDemo';
 
 const App = () => (
   <div className="App">
-    <h1>PARTICLES TRACKER</h1>
+    <span className="legend">Move Your Mouse Around</span>
     <ParticlesPointerTracker id={"particles-demo"}/>
+    <div className="content">
+      <h1>PARTICLES TRACKER</h1>
+      <p> This is a javascript library to generate a group of particles to track and follow the mouse pointer.</p>
+      <p> You can configure it by using the controls in the top right.</p>
+      <a href="https://github.com/daniel-z/particles-pointer-tracker/blob/master/src/particles-pointer-tracker.js">See Code</a>
+    </div>
   </div>
 );
 
